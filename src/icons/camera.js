b08cac98d5e1b19cb2b90954f8df93ae92b84f7c
jlarralde <Joaquin.Larralde@ice.com>
2023-12-04 18:10:10 -0300

    update gallery section

---
+++ src/icons/camera.js
@@ -0,0 +1,103 @@
+import React from "react";
+
+export default function Camera() {
+  return (
+    <svg
+      width="56"
+      height="60"
+      viewBox="0 0 56 60"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clip-path="url(#clip0_1_779)">
+        <path
+          d="M53.0845 27.7421H2.91541C1.75947 27.7421 0.822388 28.6791 0.822388 29.8351V57.2854C0.822388 58.4413 1.75947 59.3784 2.91541 59.3784H53.0845C54.2405 59.3784 55.1776 58.4413 55.1776 57.2854V29.8351C55.1776 28.6791 54.2405 27.7421 53.0845 27.7421Z"
+          stroke="#BEAA8E"
+          stroke-width="0.98"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M13.5581 32.1691H5.99785C5.40937 32.1691 4.93231 32.6462 4.93231 33.2347V37.649C4.93231 38.2375 5.40937 38.7146 5.99785 38.7146H13.5581C14.1466 38.7146 14.6236 38.2375 14.6236 37.649V33.2347C14.6236 32.6462 14.1466 32.1691 13.5581 32.1691Z"
+          stroke="#BEAA8E"
+          stroke-width="0.98"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M28.0063 54.4947C33.5338 54.4947 38.0148 50.0138 38.0148 44.4862C38.0148 38.9587 33.5338 34.4778 28.0063 34.4778C22.4788 34.4778 17.9979 38.9587 17.9979 44.4862C17.9979 50.0138 22.4788 54.4947 28.0063 54.4947Z"
+          stroke="#BEAA8E"
+          stroke-width="0.98"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M28.0063 51.6787C24.0486 51.6787 20.8139 48.4567 20.8139 44.4863C20.8139 40.5159 24.0359 37.2939 28.0063 37.2939C31.9767 37.2939 35.1987 40.5159 35.1987 44.4863C35.1987 48.4567 31.9767 51.6787 28.0063 51.6787Z"
+          stroke="#BEAA8E"
+          stroke-width="0.98"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M39.7907 27.7421H16.2093L18.0867 22.9852H37.926L39.7907 27.7421Z"
+          stroke="#BEAA8E"
+          stroke-width="0.98"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M49.6215 27.7421H44.8773V25.37C44.8773 24.0634 45.9429 22.9979 47.2494 22.9979C48.556 22.9979 49.6215 24.0634 49.6215 25.37V27.7421Z"
+          stroke="#BEAA8E"
+          stroke-width="0.98"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M18.2896 9.00635H37.723C38.5856 9.00635 39.2833 9.70402 39.2833 10.5666V18.4059C39.2833 19.2685 38.5856 19.9662 37.723 19.9662H18.2896C17.427 19.9662 16.7294 19.2685 16.7294 18.4059V10.5666C16.7294 9.70402 17.427 9.00635 18.2896 9.00635Z"
+          stroke="#BEAA8E"
+          stroke-width="0.98"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M16.7294 14.981H20.1543L22.8055 12.9894L25.4947 15.019L28.0951 13.0528L30.6575 14.9936L33.3467 12.9641L35.8457 14.8414H39.2706"
+          stroke="#BEAA8E"
+          stroke-width="0.98"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M7.72302 13.3446L12.4672 13.5602"
+          stroke="#BEAA8E"
+          stroke-width="0.98"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M8.40802 5.46722L13.1776 8.33403"
+          stroke="#BEAA8E"
+          stroke-width="0.98"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M14.4461 0.621582L16.8562 4.71883"
+          stroke="#BEAA8E"
+          stroke-width="0.98"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_1_779">
+          <rect
+            width="55.5983"
+            height="60"
+            fill="white"
+            transform="translate(0.200836)"
+          />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
--- src/view/Gallery/index.js
+++ src/view/Gallery/index.js
@@ -10,6 +10,8 @@ import photo4 from "../../images/carousel/4.jpeg";
 import photo5 from "../../images/carousel/5.jpeg";
 import photo7 from "../../images/carousel/7.jpeg";
 import photo8 from "../../images/carousel/8.jpeg";
+import greenGallery from "../../images/green_gallery.png";
+import Camera from "../../icons/camera";
 
 const photos = [photo1, photo2, photo3, photo4, photo5, photo7, photo8];
 
@@ -17,8 +19,14 @@ export default function Gallery() {
   return (
     <Section secondaryBg>
       <div>
-        <h1>Retratos de Nuestro Amor</h1>
-        <p>Un minuto, un segundo, un instante que queda en la eternidad</p>
+        <div className={styles.greenImg}>
+          <img src={greenGallery} alt="" />
+        </div>
+        <div className={styles.header}>
+          <Camera />
+          <h1>Retratos de Nuestro Amor</h1>
+          <h2>Un minuto, un segundo, un instante que queda en la eternidad</h2>
+        </div>
 
         <div className={styles.carousel}>
           <Carousel>
