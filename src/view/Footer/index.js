348bb3c8553c50a78059364e2570bbab57b6e9f7
jlarralde <Joaquin.Larralde@ice.com>
2023-12-04 19:45:39 -0300

    finish desktop

--- src/view/Footer/index.js
+++ src/view/Footer/index.js
@@ -2,19 +2,21 @@ import React from "react";
 import SaveDate from "../../components/SaveDate";
 import Section from "../../components/Section";
 import styles from "./Footer.module.scss";
+import initialsFooter from "../../images/initials_footer.svg";
 
 export default function Footer() {
   return (
-    <Section secondaryBg>
+    <Section>
       <div className={styles.container}>
-        <SaveDate />
+        <img src={initialsFooter} alt="" />
+        {/* <SaveDate />
         <div className={styles.optionsContainer}>
           <p>Confirmar asistencia a ceremonia</p>
           <p>Confirmar asistencia a fiesta</p>
           <p>Sugerir canción</p>
           <p>Agendar Fiesta</p>
           <p>Agendar Ceremonia</p>
-        </div>
+        </div> */}
       </div>
     </Section>
   );
--- src/view/SocialMedia/index.js
+++ src/view/SocialMedia/index.js
@@ -3,19 +3,22 @@ import Section from "../../components/Section";
 import Button from "../../components/Button";
 import Ribbon from "../../components/Ribbon";
 import styles from "./SocialMedia.module.scss";
+import Instagram from "../../icons/instagram";
+import DecorativeTop from "../../icons/decorativeTop";
 
 export default function SocialMedia() {
   const goToInstagram = () =>
     window.open("https://www.instagram.com/juanbucca/", "_blank");
 
   return (
-    <Section>
+    <Section secondaryBg>
       <div className={styles.container}>
         <div>
+          <DecorativeTop />
           <h1>Compartimos este día junto a ti</h1>
           <p>Comparte tus fotos y videos de este hermoso día</p>
         </div>
-        <div className={styles.icon} />
+        <Instagram />
         <Ribbon>
           <a href="https://www.instagram.com/juanbucca/">#beluyjuan</a>
         </Ribbon>
