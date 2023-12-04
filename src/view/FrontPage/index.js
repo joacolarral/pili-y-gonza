9ef031bc278d6d1c7e87c0032e06798f2a782951
jlarralde <Joaquin.Larralde@ice.com>
2023-12-04 15:40:43 -0300

    added font and green img

--- src/view/FrontPage/index.js
+++ src/view/FrontPage/index.js
@@ -1,20 +1,31 @@
 import React from "react";
 import Section from "../../components/Section";
 import styles from "./FrontPage.module.scss";
-import SaveTheDate from "../../components/SaveDate";
+import initialNames from "../../images/initial_names.png";
+import greenFrontpage from "../../images/green_frontpage.png";
+import green2Frontpage from "../../images/green2_frontpage.png";
 
 export default function FrontPage() {
   return (
     <Section padding={0}>
       <div className={styles.container}>
+        <div className={styles.greenTop}>
+          <img src={greenFrontpage} alt="" />
+        </div>
         <div className={styles.contentContainer}>
+          <h1 className={styles.dateNumber}>09.03.24</h1>
+          <div className={styles.initialNames}>
+            <img src={initialNames} alt="" />
+          </div>
           <div className={styles.content}>
-            <SaveTheDate />
-            <p>
+            <h2>
               Todos somos mortales, hasta el primer beso y la segunda copa de
               vino
-            </p>
+            </h2>
           </div>
+        </div>
+        <div className={styles.greenBottom}>
+          <img src={green2Frontpage} alt="" />
         </div>
       </div>
     </Section>
