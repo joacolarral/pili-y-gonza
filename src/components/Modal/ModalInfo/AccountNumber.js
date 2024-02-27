ed894d2a935d8f262c7f02502a4149bfd218a9b7
jlarralde <Joaquin.Larralde@ice.com>
2024-02-27 17:32:26 -0300

    feat: changed account number

--- src/components/Modal/ModalInfo/AccountNumber.js
+++ src/components/Modal/ModalInfo/AccountNumber.js
@@ -5,9 +5,9 @@ import GiftModal from "../../../icons/gitf_modal";
 export default function AccountNumber() {
   return (
     <ModalInfo title="Regalo" icon={<GiftModal />}>
-      <p> CBU: 0720429088000036008873 </p>
-      <p> Alias: Piligonza16324</p>
-      <p> Banco: Santander</p>
+      <p> CBU: 0070327530004074040383 </p>
+      <p> Alias: PILIYGONZA16M</p>
+      <p> Banco: Galicia</p>
     </ModalInfo>
   );
 }
